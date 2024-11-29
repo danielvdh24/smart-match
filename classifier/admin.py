945b00e30a5c42f50e77b92c2221c5eb2d6b2df3
Kai Rowley <rowley@student.chalmers.se>
2024-11-29 16:30:19 +0100

    #14 added admin page with login functionality
    admin able to upload the contents of a single resume via text input - of which thereupon will be added to the database

--- classifier/admin.py
+++ classifier/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from . import models
+from django.contrib.admin.apps import AdminConfig
 
-# Register your models here.
+# Create a custom AdminConfig class to specify a custom admin site
+class ClassifierAdminConfig(AdminConfig):
+    default_site = "blog.admin.ClassifierAdminArea"
+
+# Create a custom AdminSite
+class ClassifierAdminArea(admin.AdminSite):
+    site_header = "Classifier admin area"
+
+# Create an instance of the custom AdminSite
+classifier_site = ClassifierAdminArea(name="ClassifierAdmin")
+
+# Register the model with the custom admin site
+classifier_site.register(models.Resume)
+
+# If you want to add more models to the custom admin site, you can do so here
+# classifier_site.register(models.YourModel)
--- resume_classification/urls.py
+++ resume_classification/urls.py
@@ -15,10 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
-from django.contrib import admin
+#from django.contrib import admin
 from django.urls import path, include
+from classifier.admin import classifier_site
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path("admin/", classifier_site.urls),
     path("", include("classifier.urls"))
 ]
+
+# path("admin/", admin.site.urls),
