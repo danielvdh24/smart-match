30fc7b5577c9d84736d8877b126d8331c14a7c03
Mika VM <rannisto@student.chalmers.se>
2024-12-09 12:39:45 +0100

    #18 App deployed
    Added config files for kubectl to host app online, css not being served since its in production mode now, will have to add apache

--- resume_classification/settings.py
+++ resume_classification/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-owyr82&s+)rft94)m-h!rnbjs$4os#*u-#=hx3v*&2ucr758!g"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
